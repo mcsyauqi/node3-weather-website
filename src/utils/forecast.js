07b94506bde909f28b1339018375ca5d65d21017
Ahmad Thariq Syauqi <ahmadthariqsyauqi@gmail.com>
2019-05-07 22:30:29 +0700

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/17ae17408409e68c79f65c4360ca5d53/' + latitude + ',' + longitude
+
+    request({
+        url,
+        json: true
+    }, (error, {
+        body
+    }) => {
+        if (error) {
+            callback('Unable to connect!', undefined)
+        } else if (body.error) {
+            callback('Location not found!', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
